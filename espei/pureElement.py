6767ab122d73b0b0d29b282a59b38b8740621aa6
amr8004 <amr8004@psu.edu>
2024-05-22 13:58:41 -0400

    task: Update emcee dependency to version 3.1.4 and updated emcee fitting

--- espei/pureElement.py
+++ espei/pureElement.py
@@ -9,6 +9,8 @@ import os
 import json
 import espei.pure_element.DB_load as PEDB
 from pycalphad.model import Define_Element
+import emcee
+
 #from espei.paramselect import fit_formation_energy
 #TODO KNOWNS
 #H,S,G autocalculate testing
@@ -81,7 +83,7 @@ def PE_AICC(nparm, nobs,rss,aicc_factor=None):
         correction = (2.0 * p**2 * k**2 + 2.0 * pk) / (n - pk - 1.0)
     aicc = aic+correction
     print('correction: ',correction)
-    print('CHECKPOINT AICC SOLVED')
+    #print('CHECKPOINT AICC SOLVED')
     return aicc
 
 def Cp_fit(func, initialGuess, parmNames, data_df):
@@ -130,3 +132,57 @@ def Cp_fit(func, initialGuess, parmNames, data_df):
     print('AIC:', AIC(logLik, nparm))
     print('AICC:', PE_AICC(nparm, nobs,RSS))
     return parmEsts
+
+def select_model(model_flag):
+    # Select the model to use
+    if model_flag == 'RWModelE':
+        return RWModelE
+    elif model_flag == 'CSModelE':
+        return CSModelE
+    elif model_flag == 'SRModelE':
+        return SRModelE
+    else:
+        raise ValueError("Invalid model_flag")
+
+def log_likelihood(param, T, y, model_flag):
+    # Calculate the log likelihood
+    # param:
+    model_func = select_model(model_flag)
+    model_cp = model_func(T, *param)
+    return -0.5 * np.sum((y - model_cp)**2)
+
+def log_prior(param,model_flag):
+    # Flat prior within a certain range
+    if model_flag == 'RWModelE' or 'CSModelE':
+        if 0 < param[0] < 700 and 0 < param[1] < .1 and 0 < param[2] < .1:
+            return 0.0
+        else:
+            return -np.inf
+    elif model_flag == 'SRModelE':
+        if (0 < param[0] < 700) and (0 < param[1] < 0.01) and (0 < param[2] < 0.01) and (0 < param[3]< 3000) and (0 < param[4]< 3000):
+            return 0.0
+        else:
+            return -np.inf
+    else:
+        raise ValueError("Invalid model_flag")
+
+def log_probability(param, T, y,model_flag):
+    lp = log_prior(param, model_flag)
+    if not np.isfinite(lp):
+        return -np.inf
+    return lp + log_likelihood(param, T, y, model_flag)
+
+def MCMC_fit(model_flag, initialGuess, data_df, nwalkers=12, nsteps=10000, stepRemove=1000, stepThin=15):
+    ndim = len(initialGuess)
+    pos = []
+    for i in range(nwalkers):
+        re = []
+        std = np.random.randn(len(initial_params))
+        for n,v in enumerate(initial_params):
+            va = v + 0.25*std[n]*v
+            re.append(va)
+        pos.append(np.array(re))
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, log_probability, args=(data_df.Temp, data_df.Cp, model_flag))
+    sampler.run_mcmc(pos, nsteps, progress=True)
+    samples = sampler.get_chain(flat=True, thin=stepThin, discard=stepRemove)
+    return samples
